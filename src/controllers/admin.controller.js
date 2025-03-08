f85746538072963c317054bf975317ed3162d2a4
mycro01 <faresakrout3@gmail.com>
2025-03-08 23:26:46 +0100

    delete event admin

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,3 +1,4 @@
+import Admin from '../models/admin.model.js'
 import Event from '../models/event.model.js';
 import { notifyProUser } from '../utils/notifyProUser.js';
 
@@ -24,5 +25,29 @@ export const approveEvent = async (req, res) => {
   } catch (err) {
     console.error('Error approving event:', err);
     res.status(500).json({ success: false, msg: 'Error approving event', error: err.message });
+  }
+};
+export const deleteEvent = async (req, res) => {
+  const { eventId, adminId } = req.body;
+
+  try {
+    
+    const admin = await Admin.findById(adminId);
+    if (!admin) {
+      return res.status(401).json({ success: false, msg: 'Unauthorized action' });
+    }
+
+    const event = await Event.findById(eventId);
+    if (!event) {
+      return res.status(404).json({ success: false, msg: 'Event not found' });
+    }
+
+    await event.deleteOne(); 
+
+    
+    res.status(200).json({ success: true, msg: 'Event deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting event:', error); 
+    res.status(500).json({ success: false, msg: 'Error deleting event' });
   }
 };
