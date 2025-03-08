71ded95d680bd3ccbef7da5ac07279650d631127
mycro01 <faresakrout3@gmail.com>
2025-03-08 23:24:13 +0100

    Revert "delete event admin"
    This reverts commit 0e9acb3ba67b705f521ae5c193a25b7abc2a84fa.

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { approveEvent, deleteEvent } from '../controllers/admin.controller.js';
+import { approveEvent } from '../controllers/admin.controller.js';
 const adminRouter=express.Router();
 adminRouter.post('/approveEvent',approveEvent);
-adminRouter.delete('/deleteEvent',deleteEvent);
 export default adminRouter;
