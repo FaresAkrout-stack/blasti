c12a60b736ad9fa90f5bc08299382a5bbaba938a
mycro01 <faresakrout3@gmail.com>
2025-03-08 22:30:18 +0100

    update event details

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -54,11 +54,62 @@ export const enrollEvent=async(req,res)=>{
     if (!event||event.status!=='approved') {
       return res.status(400).json({ success: false, msg: 'Event not found'});
     }
-    if(event.enrollments==event.capacity){
+    if(event.enrollments >= event.capacity){
       return res.status(400).json({success:false,msg:'event capacity is full'});
     }
     return res.status(200).json({success:true,msg:'redirecting to paiment gateway ...'});
   } catch (error) {
     res.status(500).json({success:false,msg:'error in enrollemnt proccess'});
   }
+};
+export const capacityInc=async(req,res)=>{
+  /* if paiment =>capacity++*/ 
 }
+export const updateEventDetails = async (req, res) => {
+  const {
+    eventId,
+    proUserId,
+    eventCategorie,
+    location,
+    description,
+    price,
+    capacity,
+    eventTime,
+    image,
+    video,
+  } = req.body;
+
+  try {
+    const proUser = await ProUser.findById(proUserId);
+    if (!proUser) {
+      return res.status(401).json({ success: false, msg: 'Unauthorized action: ProUser not found' });
+    }
+
+    const event = await Event.findById(eventId);
+    if (!event) {
+      return res.status(404).json({ success: false, msg: 'Event not found' });
+    }
+
+    
+    if (event.proUserId.toString() !== proUserId) {
+      return res.status(403).json({ success: false, msg: 'Unauthorized: You do not have permission to update this event' });
+    }
+
+    
+    event.eventCategorie = eventCategorie ?? event.eventCategorie;
+    event.location = location ?? event.location;
+    event.description = description ?? event.description;
+    event.price = price ?? event.price;
+    event.capacity = capacity ?? event.capacity;
+    event.eventTime = eventTime ?? event.eventTime;
+    event.image = image ?? event.image;
+    event.video = video ?? event.video;
+
+    await event.save();
+
+    res.status(200).json({ success: true, msg: 'Event updated successfully', data: event });
+  } catch (error) {
+    console.error('Error updating event:', error);
+    res.status(500).json({ success: false, msg: 'Error updating event info', error: error.message });
+  }
+};
--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { enrollEvent, publishEvent } from '../controllers/event.controller.js';
+import { enrollEvent, publishEvent, updateEventDetails } from '../controllers/event.controller.js';
 
 const EventRouter = express.Router();
 
 EventRouter.post('/publishEvent', publishEvent);
 EventRouter.post('/enrollEvent',enrollEvent);
+EventRouter.post('/updateEvent',updateEventDetails);
 
 export default EventRouter;
